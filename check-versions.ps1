df290225fe735e1df8411a33d628a0b90f1c5347
Netropolitan Academy <165105013+Netropolitan@users.noreply.github.com>
2026-02-16 16:25:42 +0000

    Delete check-versions.ps1

--- check-versions.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem 'C:\Users\HP ZBook\Documents\AutoHotkey\ai-text-tools-v1.5\*.exe' | ForEach-Object {
-    $ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($_.FullName).FileVersion
-    Write-Host "$($_.Name): $ver - Modified: $($_.LastWriteTime)"
-}
